f872593148ab8fa2998ea8f777af214ff1e025fb
Quyen Pham Khac <phamkhacquyen1996@gmail.com>
2022-04-13 16:14:47 +0700

    feat: add elb and listener

--- lib/ecs-devops-sample-stack.ts
+++ lib/ecs-devops-sample-stack.ts
@@ -4,26 +4,60 @@ import * as ecr from "aws-cdk-lib/aws-ecr";
 import * as ec2 from "aws-cdk-lib/aws-ec2";
 import * as ecs from "aws-cdk-lib/aws-ecs";
 import * as iam from "aws-cdk-lib/aws-iam";
+import * as elbv2 from "aws-cdk-lib/aws-elasticloadbalancingv2";
 
 export class EcsDevopsSampleStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
+    // Create a new repository
     const repository = new ecr.Repository(
       this,
       "ecs-devops-sandbox-repository",
       { repositoryName: "ecs-devops-sandbox-repository" }
     );
 
+    // Create a new vpc with public subnets
     const vpc = new ec2.Vpc(this, "ecs-devops-sandbox-vpc", {
+      vpcName: "ecs-devops-sandbox-vpc",
       maxAzs: 2,
+      cidr: "10.0.0.0/16",
+      subnetConfiguration: [
+        {
+          name: "ecs-devops-sandbox-subnet-public",
+          subnetType: ec2.SubnetType.PUBLIC,
+          cidrMask: 24,
+        },
+        {
+          name: "ecs-devops-sandbox-subnet-private",
+          subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+          cidrMask: 24,
+        },
+      ],
     });
 
+    // Create a new Elastic Load Balancer
+    const elb = new elbv2.ApplicationLoadBalancer(
+      this,
+      "ecs-devops-sandbox-elb",
+      {
+        vpc: vpc,
+        vpcSubnets: { subnetType: ec2.SubnetType.PUBLIC },
+      }
+    );
+
+    // Create a new Load Balancer Listener
+    const listener = elb.addListener("ecs-devops-sandbox-listener", {
+      port: 80,
+    });
+
+    // Create a new ECS cluster
     const cluster = new ecs.Cluster(this, "ecs-devops-sandbox-cluster", {
       clusterName: "ecs-devops-sandbox-cluster",
       vpc: vpc,
     });
 
+    // Create a new ECS execution role
     const executionRole = new iam.Role(
       this,
       "ecs-devops-sandbox-execution-role",
@@ -48,6 +82,7 @@ export class EcsDevopsSampleStack extends Stack {
       })
     );
 
+    // Create a new ECS task definition
     const taskDefinition = new ecs.FargateTaskDefinition(
       this,
       "ecs-devops-sandbox-task-definition",
@@ -64,6 +99,7 @@ export class EcsDevopsSampleStack extends Stack {
       }
     );
 
+    // Create a new ECS service
     const service = new ecs.FargateService(this, "ecs-devops-sandbox-service", {
       cluster: cluster,
       taskDefinition: taskDefinition,
