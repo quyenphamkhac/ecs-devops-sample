8600a4470099e36c19965ec1b6ee8c8142537313
Quyen Pham Khac <phamkhacquyen1996@gmail.com>
2022-04-14 13:34:10 +0700

    fix: minor changes

--- lib/ecs-devops-sample-stack.ts
+++ lib/ecs-devops-sample-stack.ts
@@ -1,6 +1,6 @@
 import { Stack, StackProps } from "aws-cdk-lib";
 import { Construct } from "constructs";
-import * as ecr from "aws-cdk-lib/aws-ecr";
+// import * as ecr from "aws-cdk-lib/aws-ecr";
 import * as ec2 from "aws-cdk-lib/aws-ec2";
 import * as ecs from "aws-cdk-lib/aws-ecs";
 import * as iam from "aws-cdk-lib/aws-iam";
